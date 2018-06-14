61f08b08fe258787f5ebac51cca979ad415e88f4
Adam Strzelecki <ono@java.pl>
2018-06-14 11:41:14 +0200

    Support also direct <img> copying

--- extension/background.js
+++ extension/background.js
@@ -1,8 +1,78 @@
 // Code based on SVG2PNG
 
+function drawAndCopyImage(image, scale) {
+	// Draw our image on canvas
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	canvas.width = image.width * scale;
+	canvas.height = image.height * scale;
+	ctx.drawImage(image, 0, 0, image.width * scale, image.height * scale);
+
+	// Copy to clipboard using native messaging host
+	if (true) {
+		var base64 = canvas.toDataURL().substring('data:image/png;base64,'.length);
+		chrome.runtime.sendNativeMessage('com.nanoant.chrome.clipboard',
+			{ format: 'PNG', base64: base64 },
+			function(response) {
+				if (response !== true) {
+					if (response !== undefined) {
+						alert(response);
+					} else {
+						alert('Undefined response received from native messaging host!')
+					}
+				}
+			});
+	}
+	// Download method (as it was originally in SVG2PNG)
+	if (false) {
+		var a = document.createElement('a');
+		a.download = 'figure.png';
+		a.href = canvas.toDataURL('image/png');
+		document.body.appendChild(a);
+		a.addEventListener("click", function(e) {
+			a.parentNode.removeChild(a);
+		});
+		a.click();
+	}
+	// Copy to clipboard using selection
+	if (false) {
+		var img = document.createElement('img');
+		img.onload = function() {
+			var range = document.createRange();
+			range.setStartBefore(img);
+			range.setEndAfter(img);
+			range.selectNode(img);
+			var selection = window.getSelection();
+			selection.addRange(range);
+			document.execCommand('Copy');
+			img.parentNode.removeChild(img);
+			alert('Copied');
+		};
+		img.src = canvas.toDataURL();
+		document.body.appendChild(img);
+	}
+	// Copy to clipboard using event.clipboardData
+	if (false) {
+		var png = atob(canvas.toDataURL().substring('data:image/png;base64,'.length));
+		function onCopyHandler(event) {
+			event.clipboardData.setData('image/bmp', png);
+			event.preventDefault();
+			document.removeEventListener('copy', onCopyHandler, true);
+		}
+		document.addEventListener('copy', onCopyHandler, true);
+		document.execCommand('copy');
+		alert('Copied');
+	}
+}
+
 function genericOnClick(info, tab) {
 
-	chrome.tabs.sendMessage(tab.id, "getClickedEl", function(response) {
+	chrome.tabs.sendMessage(tab.id, "getSVG2ClipboardElement", function(response) {
+
+		if (response === undefined) {
+			alert('Undefined response received from content script!');
+			return;
+		}
 
 		// Get our element raw html
 		var elementHTML = response.value;
@@ -12,77 +82,27 @@ function genericOnClick(info, tab) {
 		div.innerHTML = elementHTML;
 		var elementSVG = div.firstChild;
 
-		// Verify that we have a <svg> element
-		if (elementSVG.tagName.toLowerCase() != "svg") {
-			alert("This is not a <svg> element!");
+		// Create an image
+		var image = new Image; // Not shown on page
+		var scale = 1;
+
+		// Verify that we have a <svg> or <img> element
+		var tagName = elementSVG.tagName.toLowerCase();
+		if (tagName == "svg") {
+			var svgAsXML = (new XMLSerializer).serializeToString(elementSVG);
+			image.src = 'data:image/svg+xml,' + encodeURIComponent(svgAsXML);
+			// Use 200% for SVG
+			scale = 2;
+		} else if (tagName == "img") {
+			// Plain <img> -- just copy source
+			image.src = elementSVG.src;
+		} else {
+			alert("This is not a <svg> or <img> element!");
 			return;
 		}
 
-		// Create an image
-		var image = new Image; // Not shown on page
-		var svgAsXML = (new XMLSerializer).serializeToString(elementSVG);
-		image.src = 'data:image/svg+xml,' + encodeURIComponent(svgAsXML);
-
 		image.onload = function() {
-			// Draw our image on canvas
-			var canvas = document.createElement('canvas');
-			var ctx = canvas.getContext('2d');
-			var scale = 2;
-			canvas.width = image.width * scale;
-			canvas.height = image.height * scale;
-			ctx.drawImage(image, 0, 0, image.width * scale, image.height * scale);
-
-			// Copy to clipboard using native messaging host
-			if (true) {
-				var base64 = canvas.toDataURL().substring('data:image/png;base64,'.length);
-				chrome.runtime.sendNativeMessage('com.nanoant.chrome.clipboard',
-					{ format: 'PNG', base64: base64 },
-					function(response) {
-						if (response !== true) {
-							alert(response);
-						}
-					});
-			}
-			// Download method (as it was originally in SVG2PNG)
-			if (false) {
-				var a = document.createElement('a');
-				a.download = 'figure.png';
-				a.href = canvas.toDataURL('image/png');
-				document.body.appendChild(a);
-				a.addEventListener("click", function(e) {
-					a.parentNode.removeChild(a);
-				});
-				a.click();
-			}
-			// Copy to clipboard using selection
-			if (false) {
-				var img = document.createElement('img');
-				img.onload = function() {
-					var range = document.createRange();
-					range.setStartBefore(img);
-					range.setEndAfter(img);
-					range.selectNode(img);
-					var selection = window.getSelection();
-					selection.addRange(range);
-					document.execCommand('Copy');
-					img.parentNode.removeChild(img);
-					alert('Copied');
-				};
-				img.src = canvas.toDataURL();
-				document.body.appendChild(img);
-			}
-			// Copy to clipboard using event.clipboardData
-			if (false) {
-				var png = atob(canvas.toDataURL().substring('data:image/png;base64,'.length));
-				function onCopyHandler(event) {
-					event.clipboardData.setData('image/bmp', png);
-					event.preventDefault();
-					document.removeEventListener('copy', onCopyHandler, true);
-				}
-				document.addEventListener('copy', onCopyHandler, true);
-				document.execCommand('copy');
-				alert('Copied');
-			}
+			drawAndCopyImage(image, scale);
 		};
 	});
 }
--- extension/content.js
+++ extension/content.js
@@ -1,11 +1,11 @@
 // Code based on SVG2PNG
 
-var clickedEl = null;
+var clickedElement = null;
 
 document.addEventListener("mousedown", function(event) {
 	// right click
 	if (event.button == 2) {
-		clickedEl = event.target;
+		clickedElement = event.target;
 	}
 });
 
@@ -15,7 +15,7 @@ document.addEventListener("contextmenu", function(event) {
 	// Loop through parents until we find a svg
 	while (currentElement) {
 		if (currentElement.tagName == "svg") {
-			clickedEl = currentElement;
+			clickedElement = currentElement;
 			break;
 		} else {
 			currentElement = currentElement.parentNode;
@@ -29,13 +29,14 @@ document.addEventListener("contextmenu", function(event) {
 });
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request == "getClickedEl") {
-		// We must restore colors from inherited to inline
-		var styles = window.getComputedStyle(clickedEl);
-		clickedEl.style.fill = styles.getPropertyValue("fill");
-		clickedEl.style.stroke = styles.getPropertyValue("stroke");
-
+	if (request == "getSVG2ClipboardElement") {
+		if (clickedElement.tagName == "svg") {
+			// We must restore colors from inherited to inline
+			var styles = window.getComputedStyle(clickedElement);
+			clickedElement.style.fill = styles.getPropertyValue("fill");
+			clickedElement.style.stroke = styles.getPropertyValue("stroke");
+		}
 		// Pass element along
-		sendResponse({value: clickedEl.outerHTML});
+		sendResponse({value: clickedElement.outerHTML});
 	}
 });
