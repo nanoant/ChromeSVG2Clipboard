dfe01e8a0ce59a9729b9e00829cbad12f48721af
Adam Strzelecki <ono@java.pl>
2021-01-14 19:59:00 +0100

    ChromeClipboardHost written in C#/.NET

---
+++ ChromeClipboardHost/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ChromeClipboardHost
+{
+  class Program
+  {
+    class Data
+    {
+      [JsonPropertyName("format")]
+      public string Format { get; set; }
+      [JsonPropertyName("base64")]
+      public string Base64 { get; set; }
+    }
+
+    static private T Read<T>(BinaryReader input)
+    {
+      int length = input.ReadInt32();
+      byte[] jsonBuffer = input.ReadBytes(length);
+      return JsonSerializer.Deserialize<T>(jsonBuffer);
+    }
+
+    static private void Write<T>(BinaryWriter output, T value)
+    {
+      byte[] jsonBuffer = JsonSerializer.SerializeToUtf8Bytes<T>(value);
+      output.Write(jsonBuffer.Length);
+      output.Write(jsonBuffer);
+      output.Flush();
+    }
+
+    [DllImport("user32.dll", SetLastError=true)]
+    private static extern bool OpenClipboard(IntPtr hWndNewOwner);
+    [DllImport("user32.dll", SetLastError=true)]
+    private static extern bool EmptyClipboard();
+    [DllImport("user32.dll", SetLastError=true)]
+    private static extern uint RegisterClipboardFormat(string format);
+    [DllImport("user32.dll")]
+    private static extern IntPtr SetClipboardData(uint uFormat, IntPtr hMem);
+    [DllImport("user32.dll", SetLastError=true)]
+    private static extern bool CloseClipboard();
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    internal static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+
+    private static void WriteClipboard(string format, byte[] data)
+    {
+      OpenClipboard(IntPtr.Zero);
+      EmptyClipboard();
+      uint cformat = RegisterClipboardFormat(format);
+      IntPtr hglobal = Marshal.AllocHGlobal(data.Length);
+      Marshal.Copy(data, 0, hglobal, data.Length);
+      SetClipboardData(cformat, hglobal);
+      CloseClipboard();
+      Marshal.FreeHGlobal(hglobal);
+    }
+
+    static void Main(string[] args)
+    {
+      try
+      {
+        using (Stream inputStream = Console.OpenStandardInput(), outputStream = Console.OpenStandardOutput())
+        using (BinaryReader input = new BinaryReader(inputStream))
+        using (BinaryWriter output = new BinaryWriter(outputStream))
+        {
+          while(true)
+          {
+            Data data = Read<Data>(input);
+            try
+            {
+              WriteClipboard(data.Format, Convert.FromBase64String(data.Base64));
+              Write(output, true);
+            }
+            catch (Exception ex)
+            {
+              Write(output, ex.Message);
+              throw;
+            }
+          }
+        }
+      }
+      catch (EndOfStreamException)
+      {
+        // do nothing this is fine
+      }
+      catch (Exception ex)
+      {
+        MessageBox(IntPtr.Zero, ex.Message, "ChromeClipboardHost failed", 0x00000010);
+      }
+    }
+  }
+}
---
+++ ChromeClipboardHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChromeClipboardHost")]
+[assembly: AssemblyDescription("Clipboard host for Chrome")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("nanoANT")]
+[assembly: AssemblyProduct("ChromeClipboardHost")]
+[assembly: AssemblyCopyright("Copyright © 2021 Adam Strzelecki")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7aabde80-9343-47bf-881e-42f07c4c4c01")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
